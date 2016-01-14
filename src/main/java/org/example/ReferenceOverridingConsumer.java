1ec7c93e69af4a9a05521cc837eedba806bf9ea8
Thomas Eizinger <thomas.eizinger@senacor.com>
2016-01-14 09:19:08 +0100

    replace hardcoded true with negation of incoming value to "ensure" change log the new value

--- src/main/java/org/example/ReferenceOverridingConsumer.java
+++ src/main/java/org/example/ReferenceOverridingConsumer.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ReferenceOverridingConsumer implements Consumer<Boolean> {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public void accept(Boolean value) {
-        value = true;
+
+        final boolean newValue = !value;
+
+        LOGGER.info("Changing value to: {}", newValue);
+
+        value = newValue;
     }
 }
